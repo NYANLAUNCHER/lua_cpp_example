47dfb59e61a14d94592d085b33d621ed9019ce04
markiep <purcellmarkie@proton.me>
2026-01-04 08:38:11 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -19,18 +19,32 @@ void readfile(const std::string path, std::string& src) {
         src.resize(file.tellg());
         file.seekg(0, std::ios::beg);
         file.read(&src[0], src.size());
-        //ss << file.rdbuf();
-        //file.close();
-        //src = ss.str();
     } catch (const std::ifstream::failure& e) {
         std::cout << "Exception thrown: " << e.what() << std::endl;
     }
 }
 
+struct meshDataLayout {
+    std::string type;
+    unsigned int size;
+};
+
+struct meshData {
+    float* vertexData;// offsets are looked-up in layout
+    unsigned int indices;
+    meshDataLayout* layout;
+};
+
 int main() {
     std::string LUA_SRC;
     readfile(LUA_DIR"/test.lua", LUA_SRC);
+    // Create Lua state
     lua_State* L = luaL_newstate();
+    luaL_dostring(L, LUA_SRC.c_str());
+    lua_setglobal(L, "createMesh");// Loads lua mesh data into cpp struct
+    lua_setglobal(L, "updateMesh");// Updates cpp mesh struct
+    lua_setglobal(L, "drawMesh");// Prints cpp mesh struct
+    // Cleanup
     lua_close(L);
     return 0;
 }
