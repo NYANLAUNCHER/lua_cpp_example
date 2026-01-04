aa6b3c781199b8b28ea14ea4b274d74458d79b8f
markiep <purcellmarkie@proton.me>
2026-01-04 17:44:15 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@
 // Local
 #include "_generated/sys_paths.h"
 
-int gMESH_INDEX=0;
-
 void readfile(const std::string path, std::string& src) {
     std::ifstream file;
     std::stringstream ss;
@@ -45,42 +43,71 @@ struct MeshData {
     Attrib* attributes;
 };
 
+static int gMESH_INDEX=0;
+static MeshData* gMESH_DATA;
+
 // Cpp functions to call from lua
 // Load mesh data into a cpp struct
 // returns mesh_id
-auto createMesh = [&gMESH_INDEX](lua_State* L) -> int {
-    printf("Hello from createMesh()!\n");
+auto createMesh = [](lua_State* L) -> int {
+    // args: Attrib* layout, MeshData mesh
     gMESH_INDEX++;
+    MeshData mesh=gMESH_DATA[gMESH_INDEX];
+    // return mesh_id
     lua_pushnumber(L, gMESH_INDEX);
     return 1;// number of return values
 };
 // Print out mesh data
 auto drawMesh = [](lua_State* L) -> int {
-    printf("Hello from drawMesh()!");
     return 0;// number of return values
 };
 // Update mesh data
 auto updateMesh = [](lua_State* L) -> int {
-    printf("Hello from updateMesh()!");
     return 0;// number of return values
 };
 
 int main() {
     std::string lua_src = R"(
-    meshIdx = createMesh()
+    local layout = {
+      {
+        type="position",
+        size=2
+      },
+      {
+        type="color",
+        size=3
+      },
+    }
+    local mesh = {
+      --position    color
+      {-0.5, 0.5,  1.0, 0.0, 0.0}, -- Top left (1)
+      { 0.5, 0.5,  0.0, 1.0, 0.0}, -- Top right (2)
+      { 0.5,-0.5,  0.0, 0.0, 1.0}, -- Bottom Right (3)
+      {-0.5,-0.5,  0.0, 0.0, 0.0}, -- Bottom Left (4)
+      -- EBO Data:
+      -- Note index starts with 1
+      indices={-- using CW winding order
+        1, 2, 3, -- upper-right triangle
+        1, 3, 4  -- lower-left triangle
+      }
+    }
+    createMesh(layout, mesh)
+    meshIdx = createMesh(layout, mesh)
     )";
     //readfile(LUA_DIR"/test.lua", lua_src);
     // Create Lua state
     lua_State* L = luaL_newstate();
+    // Push globals
     lua_pushcfunction(L, createMesh);
     lua_setglobal(L, "createMesh");
     lua_pushcfunction(L, updateMesh);
+    lua_setglobal(L, "updateMesh");
+    lua_pushcfunction(L, drawMesh);
+    lua_setglobal(L, "drawMesh");
+    // Run Lua File
+    luaL_dostring(L, lua_src.c_str());
     lua_getglobal(L, "meshIdx");
     printf("meshIdx = %d\n", (int)lua_tointeger(L, -1));
-    //lua_setglobal(L, "updateMesh");
-    //lua_pushcfunction(L, drawMesh);
-    //lua_setglobal(L, "drawMesh");
-    luaL_dostring(L, lua_src.c_str());
     // Cleanup
     lua_close(L);
     return 0;
