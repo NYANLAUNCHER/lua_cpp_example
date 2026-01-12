5a2425f8ef1d4418a6457dc8c318dfd7116c5305
markiep <purcellmarkie@proton.me>
2026-01-11 20:42:18 -0600

    Ignoring generated headers

--- src/_generated/sys_paths.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define RESOURCE_DIR "/home/markiep/src/.private/lua_cpp_example/outputs/out/share/lua_cpp_example"
-#define SHADER_DIR RESOURCE_DIR"/shaders"
-#define TEXTURE_DIR RESOURCE_DIR"/textures"
-#define LUA_DIR RESOURCE_DIR"/lua"
